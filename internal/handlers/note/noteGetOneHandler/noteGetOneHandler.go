5ed7657d33d0aeb039242a63d6e88c468b474813
Maksim Nikolaev <0375nikoly@mail.ru>
2026-02-19 22:20:36 +0300

    19.02.2026

--- internal/handlers/note/noteGetOneHandler/noteGetOneHandler.go
+++ internal/handlers/note/noteGetOneHandler/noteGetOneHandler.go
@@ -1,19 +1,22 @@
-package notGetHandler
+package noteGetOneHandler
 
 import (
-	mid "note_service/internal/middleware"
-
 	"log/slog"
 	"net/http"
+	mid "note_service/internal/middleware"
+	"strconv"
 	"time"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/render"
 )
 
 type Request struct {
 	UserID int `json:"user_id"`
+	NoteID int `json:"note_id"`
 }
+
 type Note struct {
 	ID        int       `json:"id"`
 	UserID    int       `json:"user_id"`
@@ -24,17 +27,17 @@ type Note struct {
 }
 
 type Response struct {
-	Notes []Note `json:"notes"`
+	Note Note `json:"note"`
 }
 
-type NotGetHandler interface {
-	NoteGet(req Request) (Response, error)
+type NoteGetOneHandler interface {
+	NoteGetOne(req Request) (Response, error)
 }
 
-func New(log *slog.Logger, NotGetHandler NotGetHandler) http.HandlerFunc {
+func New(log *slog.Logger, NoteGetOneHandler NoteGetOneHandler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		defer r.Body.Close()
-		const op = "internal/handlers/note/notGetHandler/NoteGet"
+		const op = "handlers/note/noteGetOneHandler"
 		log := log.With(
 			slog.String("operation", op),
 			slog.String("request_id", middleware.GetReqID(r.Context())))
@@ -46,16 +49,24 @@ func New(log *slog.Logger, NotGetHandler NotGetHandler) http.HandlerFunc {
 			render.JSON(w, r, map[string]string{"error": "user not authorized"})
 			return
 		}
-		req := Request{UserID: userID}
-		resp, err := NotGetHandler.NoteGet(req)
+		noteIDStr := chi.URLParam(r, "id") // если роут: /notes/{id}
+		noteID, err := strconv.Atoi(noteIDStr)
 		if err != nil {
-			log.Error("failed to get notes", slog.String("error", err.Error()))
+			render.Status(r, http.StatusBadRequest)
+			render.JSON(w, r, map[string]string{"error": "invalid note id"})
+			return
+		}
+		req := Request{UserID: userID, NoteID: noteID}
+
+		resp, err := NoteGetOneHandler.NoteGetOne(req)
+		if err != nil {
+			log.Error("failed to get note", slog.String("error", err.Error()))
 			render.Status(r, http.StatusInternalServerError)
-			render.JSON(w, r, map[string]string{"error": "failed to get notes"})
+			render.JSON(w, r, map[string]string{"error": "failed to get note"})
 			return
 		}
 		render.Status(r, http.StatusOK)
 		render.JSON(w, r, resp)
-		log.Info("notes retrieved successfully")
+		log.Info("note retrieved successfully")
 	}
 }
