ecd748dd393fe34337a1d873eef7fe92f3f37c6e
Maksim Nikolaev <0375nikoly@mail.ru>
2026-02-19 23:32:25 +0300

    19.02.2026.

--- internal/handlers/note/NoteDeleteHandler/NoteDeleteHandler.go
+++ internal/handlers/note/NoteDeleteHandler/NoteDeleteHandler.go
@@ -5,18 +5,28 @@ import (
 	"net/http"
 	mid "note_service/internal/middleware"
 	"strconv"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/render"
 )
 
+type resultNote struct {
+	Id         int
+	UserId     int
+	Title      string
+	Content    string
+	Created_at time.Time
+	Updated_at time.Time
+}
 type Request struct {
 	NoteID int `json:"note_id"`
 }
 
 type NoteDeleteHandler interface {
 	NoteDelete(noteID int) error
+	NoteGetOne(noteID int) (resultNote, error)
 }
 
 func New(log *slog.Logger, NoteDeleteHandler NoteDeleteHandler) http.HandlerFunc {
@@ -26,7 +36,7 @@ func New(log *slog.Logger, NoteDeleteHandler NoteDeleteHandler) http.HandlerFunc
 			slog.String("operation", op),
 			slog.String("request_id", middleware.GetReqID(r.Context())))
 
-		_, ok := r.Context().Value(mid.UserIDKey).(int)
+		userID, ok := r.Context().Value(mid.UserIDKey).(int)
 		if !ok {
 			log.Error("UserID not found in context")
 			render.Status(r, http.StatusInternalServerError)
@@ -48,6 +58,20 @@ func New(log *slog.Logger, NoteDeleteHandler NoteDeleteHandler) http.HandlerFunc
 			render.JSON(w, r, map[string]string{"error": "note_id must be greater than 0"})
 			return
 		}
+		//Отсюда начали проверять, принадлежит ли заметка данному пользователю
+		note, err := NoteDeleteHandler.NoteGetOne(noteID)
+		if err != nil {
+			render.Status(r, http.StatusNotFound)
+			render.JSON(w, r, map[string]string{"error": "note not found"})
+			return
+		}
+
+		if note.UserId != userID {
+			render.Status(r, http.StatusForbidden) // 403
+			render.JSON(w, r, map[string]string{"error": "access forbidden"})
+			return
+		}
+		//Здесь закончили проверять пользователя и заметку, для этого создали структуру resultNote
 
 		err = NoteDeleteHandler.NoteDelete(noteID)
 		if err != nil {
